a643e1c1b4cf80ec464266a46e970eeac02939b5
Salem Harrache <salem.harrache@inria.fr>
2015-09-14 19:00:29 +0200

    Build all recipes with cache

--- build.sh
+++ build.sh
@@ -17,7 +17,8 @@ for RECIPE_PATH in `ls qemu/*.yaml`; do
     RECIPE_NAME=$(basename $RECIPE_PATH .yaml)
     if [ ! -e "$ROOTFS_PATH/${RECIPE_NAME}.tar.xz" ]; then
         rm -rf $BUILD_PATH
-        (set -x; kameleon build $ROOT_PROJECT/$RECIPE_PATH --build-path $BUILD_PATH --script)
+
+        (set -x; kameleon build $ROOT_PROJECT/$RECIPE_PATH --build-path $BUILD_PATH --script --cache-archive-compression=xz --enable-cache)
         if [ $? -eq 0 ]; then
             mkdir -p $ROOTFS_PATH
             mv $BUILD_PATH/$RECIPE_NAME/*.tar.xz $ROOTFS_PATH/
