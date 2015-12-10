4188e1909f9241c7143d4c5f746869557929d312
Salem Harrache <salem.harrache@inria.fr>
2015-12-10 11:04:00 +0100

    No need to compress polipo cache archive

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ for RECIPE_PATH in `ls qemu/*.yaml`; do
     if [ ! -e "$ROOTFS_PATH/${RECIPE_NAME}.tar.xz" ]; then
         rm -rf $BUILD_PATH
 
-        (set -x; kameleon build $ROOT_PROJECT/$RECIPE_PATH --build-path $BUILD_PATH --script --cache-archive-compression=xz --enable-cache)
+        (set -x; kameleon build $ROOT_PROJECT/$RECIPE_PATH --build-path $BUILD_PATH --script --enable-cache)
         if [ $? -eq 0 ]; then
             mkdir -p $ROOTFS_PATH
             mv $BUILD_PATH/$RECIPE_NAME/*.tar.xz $ROOTFS_PATH/
